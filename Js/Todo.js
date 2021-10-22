b54eba1efbabfe19984749f47c61dc51f1b6e52f
VETRIVELSRINIVASAN <vetrivel.s@e5.ai>
2021-10-22 17:26:27 +0530

    todo

--- Js/Todo.js
+++ Js/Todo.js
@@ -29,3 +29,25 @@ function myTask() {
   firstMain[0].innerHTML = "Tasks";
   secondMain[0].innerHTML = "+ Add a task";
 }
+function showTask() {
+  let input = document.getElementById("new-list");
+  let addedTask = document.getElementById("added-task");
+  var newlist = document.createElement("p");
+  newlist.classList.add("new-styling");
+  newlist.innerText = input.value;
+  addedTask.appendChild(newlist);
+  input.value = "";
+  let firstMain = document.getElementsByClassName("first-main");
+  let secondMain = document.getElementsByClassName("second-main");
+  firstMain[0].innerHTML = newlist.innerText;
+  secondMain[0].innerHTML = "+ Add a task to"+ newlist.innerText;
+  newlist.addEventListener('click', function(){
+    firstMain[0].innerHTML = newlist.innerText;
+    secondMain[0].innerHTML = "+ Add a task to"+ newlist.innerText;
+    })
+  newlist.addEventListener('dblclick', function(){
+    addedTask.removeChild(newlist);
+    firstMain[0].innerHTML = "Tasks";
+    secondMain[0].innerHTML = "+ Add a task ";
+  })
+}
